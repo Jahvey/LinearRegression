c346c029df4cc3f62bedaa262e86f41578d93b94
Emmanuel Lodovice <name3anad@gmail.com>
2014-10-19 00:05:11 +0800

    for testing

--- src/Tester.java
+++ src/Tester.java
@@ -1,6 +1,6 @@
 public class Tester {
     public static void main(String[] args) throws Exception {
-        LinearRegression lr = new LinearRegression("test.in", null);
-        
+        LinearRegression lr = new LinearRegression("DataSet/winequality-red.csv", null);
+        lr.gradientDescend();
     }
 }
